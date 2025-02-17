0685a24d1f486eb110d137fa62e78c3950bc46c2
PeakCoder <spencer.wong@hotmail.com>
2025-02-17 10:33:37 +0800

    chore: default theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,6 +61,11 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: 'Shadertoy 教程',
